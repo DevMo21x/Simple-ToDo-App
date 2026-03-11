c43b81f540260827a09796bbe4bb74c542734563
DevMo.21x <DevMo.21x@outlook.com>
2026-03-10 21:45:20 -0300

    feat: enhance TagFilter component with task count and dynamic styling

--- frontend/src/pages/app/menu/MenuFilterBoxes.tsx
+++ frontend/src/pages/app/menu/MenuFilterBoxes.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import TagFiler from "./TagFilter";
 
 const MenuFilterBoxes = () => {
-  const [selectedFilter, setSelectedFilter] = useState<string | null>(null); 
+  const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
 
   const handleFilterClick = (filter: string) => {
     setSelectedFilter(filter);
@@ -12,55 +12,72 @@ const MenuFilterBoxes = () => {
 
   return (
     <>
-    <div className="menu-filter-boxes-container mt-5">
-      <div className="menu-p-container mb-5">
-        <p>Menu</p>
-      </div>
-      <div className="flex flex-col gap-2.5">
-        <FilterBox
-          Icon={Inbox}
-          text="All Tasks"
-          numberOfTasks={3}
-          isSelected={selectedFilter === "All Tasks"} 
-          onClick={() => handleFilterClick("All Tasks")}
-        />
-        <FilterBox
-          Icon={Calendar}
-          text="Today"
-          numberOfTasks={5}
-          isSelected={selectedFilter === "Today"}
-          onClick={() => handleFilterClick("Today")}
-        />
-        <FilterBox
-          Icon={Star}
-          text="Important"
-          numberOfTasks={2}
-          isSelected={selectedFilter === "Important"}
-          onClick={() => handleFilterClick("Important")}
-        />
-        <FilterBox
-          Icon={Archive}
-          text="Completed"
-          numberOfTasks={13}
-          isSelected={selectedFilter === "Completed"}
-          onClick={() => handleFilterClick("Completed")}
-        />
-      </div>
-    </div>
-    
-    {/* Tags container */}
-    <div className="tags-container mt-10">
-      <div>
-        <p>TAGS</p>
-      </div>
-      <div>
-        <TagFiler filterName='Urgent'/>
-
+      <div className="menu-filter-boxes-container mt-5">
+        <div className="menu-p-container mb-5">
+          <p>Menu</p>
+        </div>
+        <div className="flex flex-col gap-2.5">
+          <FilterBox
+            Icon={Inbox}
+            text="All Tasks"
+            numberOfTasks={3}
+            isSelected={selectedFilter === "All Tasks"}
+            onClick={() => handleFilterClick("All Tasks")}
+          />
+          <FilterBox
+            Icon={Calendar}
+            text="Today"
+            numberOfTasks={5}
+            isSelected={selectedFilter === "Today"}
+            onClick={() => handleFilterClick("Today")}
+          />
+          <FilterBox
+            Icon={Star}
+            text="Important"
+            numberOfTasks={2}
+            isSelected={selectedFilter === "Important"}
+            onClick={() => handleFilterClick("Important")}
+          />
+          <FilterBox
+            Icon={Archive}
+            text="Completed"
+            numberOfTasks={13}
+            isSelected={selectedFilter === "Completed"}
+            onClick={() => handleFilterClick("Completed")}
+          />
+        </div>
       </div>
 
-    </div>
-      </>
-
+      {/* Tags container */}
+      <div className="tags-container mt-10 flex flex-col items-start">
+        <div>
+          <p>TAGS</p>
+        </div>
+        <div>
+          <TagFiler
+            filterName="Urgent"
+            tagColor={"#FF6466"}
+            numberOfTasks={3}
+            isSelected={selectedFilter === "Urgent"}
+            onClick={() => handleFilterClick("Urgent")}
+          />
+          <TagFiler
+            filterName="Personal"
+            tagColor={"#50A2FF"}
+            numberOfTasks={3}
+            isSelected={selectedFilter === "Personal"}
+            onClick={() => handleFilterClick("Personal")}
+          />
+          <TagFiler
+            filterName="Work"
+            tagColor={"#01DF73"}
+            numberOfTasks={3}
+            isSelected={selectedFilter === "Work"}
+            onClick={() => handleFilterClick("Work")}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
--- frontend/src/pages/app/menu/TagFilter.tsx
+++ frontend/src/pages/app/menu/TagFilter.tsx
@@ -2,16 +2,43 @@ import { Link } from "react-router-dom";
 
 interface TagFilterProps {
   filterName: string;
+  tagColor: string;
+  numberOfTasks: number;
+  isSelected: boolean;
+  onClick: () => void;
 }
-const TagFiler: React.FC<TagFilterProps> = ({ filterName }) => {
+
+const TagFiler: React.FC<TagFilterProps> = ({
+  filterName,
+  tagColor,
+  numberOfTasks,
+  isSelected,
+  onClick,
+}) => {
+  const boxStyle = {
+    backgroundColor: isSelected ? "#F1F5F9" : undefined,
+    boxShadow: isSelected
+      ? "inset 2px 2px 5px rgba(0, 0, 0, 0.2), inset -2px -2px 5px rgba(255, 255, 255, 0.5)"
+      : "2px 2px 5px rgba(0, 0, 0, 0.2), -2px -2px 5px rgba(255, 255, 255, 0.5)",
+    transform: isSelected ? "scale(0.98)" : "scale(1)",
+    transition: "transform 0.1s ease-in-out, box-shadow 0.1s ease-in-out",
+  };
   return (
-    <div className="max-w-87.5 p-1.5 hover:bg-gray-100 rounded-sm">
+    <div
+      style={boxStyle}
+      onClick={onClick}
+      className="w-87.5 p-1.5 hover:bg-gray-100 rounded-sm pt-3"
+    >
       <Link to="">
         <div className="flex justify-between">
           <div className="flex gap-3">
-            <p>{}</p>
+            <div
+              className="border w-6 rounded-4xl"
+              style={{ backgroundColor: tagColor }}
+            ></div>
+            <p>{filterName}</p>
           </div>
-          <p className={`mr-3 `}>{}</p>
+          <p className="">{numberOfTasks}</p>
         </div>
       </Link>
     </div>
