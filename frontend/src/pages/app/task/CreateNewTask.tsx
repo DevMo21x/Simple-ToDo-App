2dbcc10bd14d2802f417c5d5404e39a3bc00e40e
DevMo.21x <DevMo.21x@outlook.com>
2026-03-11 22:51:14 -0300

    feat: implement CreateNewTask component with styling and integrate into TasksContentLayout

--- frontend/src/pages/app/task/CreateNewTask.tsx
+++ frontend/src/pages/app/task/CreateNewTask.tsx
@@ -1,9 +1,17 @@
+import { Plus } from "lucide-react";
 const CreateNewTask = () => {
   return (
-    <div>
+    <div className="flex justify-between create-task-container p-5 border-dashed border-2 border-gray-300 rounded-2xl cursor-pointer hover:border-gray-400 transition-colors duration-300 ease-in-out">
+      <div className="flex gap-5">
+        <div className="radio-btn-container border-dashed border-2 border-gray-300 w-6 rounded-4xl h-6"></div>
+        <p className="text-[#AFB1B7]">Add a new task...</p>
+      </div>
 
+      <div>
+        <Plus color="#AFB1B7" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateNewTask
+export default CreateNewTask;
--- frontend/src/pages/app/task/TasksContentLayout.tsx
+++ frontend/src/pages/app/task/TasksContentLayout.tsx
@@ -1,10 +1,13 @@
 import TasksHeading from "./TasksHeading";
 import TaskContainer from "./TaskContainer";
-
+import CreateNewTask from "./CreateNewTask";
 const TasksMainLayout = () => {
   return (
     <>
       <TasksHeading />
+      <div className="mt-5">
+      <CreateNewTask />
+      </div>
       <div className="mt-5">
         <TaskContainer />
       </div>
